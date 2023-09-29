88a0f7b39054533cf0457e8e9f1b75b6f5f3a1a4
Kylian Dekker <kdekker@climax.local>
2023-09-29 16:31:42 +0100

    Started with return data for scripts

---
+++ Humongous File Editor/include/lowlevel/bytecode_data.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+namespace HumongousFileEditor
+{
+	namespace chunk_reader
+	{
+        struct bytecode_data
+        {
+            std::vector<uint8_t> data_8;
+            std::vector<uint16_t> data_16;
+            std::vector<uint32_t> data_32;
+            std::vector<std::string> data_str;
+        };
+	}
+}
--- Humongous File Editor/src/functions/types/SCRPTab.cpp
+++ Humongous File Editor/src/functions/types/SCRPTab.cpp
@@ -5,14 +5,17 @@
 #include "lowlevel/FileContainer.h"
 #include "lowlevel/utils.h"
 #include "lowlevel/OPCodes.h"
+#include "lowlevel/bytecode_data.h"
 
 namespace HumongousFileEditor
 {
 	struct instruction
 	{
+		uint8_t code;
 		std::string name;
 		size_t offset;
 		size_t size;
+		chunk_reader::bytecode_data data;
 	};
 	bool SCRPTab::GetData(chunk_reader::FileContainer*& fc, size_t offset)
 	{
@@ -25,16 +28,25 @@ namespace HumongousFileEditor
 		std::vector<instruction> instructions;
 		while (pos < scrp_size)
 		{
-			size_t offset = pos;
+			size_t bc_offset = pos;
 			uint8_t b = scrp_chunk.data[pos];
 
+			instruction instruction;
+			instruction.code = b;
+
 			chunk_reader::bytecode bytecode = chunk_reader::OPCODES_HE90[b];
-			std::string bc = bytecode.name;
+			instruction.name = bytecode.name;
 			pos++;
 
-			size_t bytecode_size = bytecode.func(utils::add(fc->data, offset + sizeof(chunk_reader::HumongousHeader) + pos), scrp_size - pos);
+			chunk_reader::bytecode_data bcdata;
+
+			size_t bytecode_size = bytecode.func(utils::add(fc->data, offset + sizeof(chunk_reader::HumongousHeader) + pos), scrp_size - pos, bcdata);
 			pos += bytecode_size;
-			instructions.push_back({ bc , offset, bytecode_size });
+
+			instruction.offset = bc_offset;
+			instruction.size = bytecode_size;
+			instruction.data = bcdata;
+			instructions.push_back(instruction);
 		}
 
 		return false;
